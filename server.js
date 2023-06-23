72b3ca0a0b1069eecf40928b027f4b312f084c2f
Subodh <suboodhsalve2001@gmail.com>
2023-06-23 17:09:36 +0530

    ok

--- server.js
+++ server.js
@@ -4,8 +4,14 @@ const cors = require("cors")
 const projectRoute = require("./routes/projectRoute")
 require("dotenv").config({ path: "./config/.env" })
 const app = express()
-app.use(cors())
+app.use(cors({
+    credentials: true,
+    origin: (o, cb) => {
+        const allowed = "https://subodh-portfolio.up.railway.app"
+    }
+}))
 app.use(express.static("public"))
+// app.use()
 app.use(express.json())
 app.use("/projects", projectRoute)
 connectDB()
