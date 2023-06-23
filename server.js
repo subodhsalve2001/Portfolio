b750927004d2b7911a4cec527c8bbbc19ab62441
Subodh <suboodhsalve2001@gmail.com>
2023-06-23 17:39:45 +0530

    updaye

--- server.js
+++ server.js
@@ -4,12 +4,14 @@ const cors = require("cors")
 const projectRoute = require("./routes/projectRoute")
 require("dotenv").config({ path: "./config/.env" })
 const app = express()
-app.use(cors({
-    credentials: true,
-    origin: (o, cb) => {
-        const allowed = "https://subodh-portfolio.up.railway.app"
-    }
-}))
+app.use(cors())
+// app.use(cors({
+//     credentials: true,
+//     origin: (o, cb) => {
+//         // const allowed = "https://subodh-portfolio.up.railway.app"
+//         const allowed = "http://localhost:5000"
+//     }
+// }))
 app.use(express.static("public"))
 // app.use()
 app.use(express.json())
