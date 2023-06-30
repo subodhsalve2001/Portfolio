5b40228fc11d17ea54370061b82e71d7873dd995
Subodh <suboodhsalve2001@gmail.com>
2023-06-30 10:00:41 +0530

    initial commit

--- server.js
+++ server.js
@@ -5,13 +5,6 @@ const projectRoute = require("./routes/projectRoute")
 require("dotenv").config({ path: "./config/.env" })
 const app = express()
 app.use(cors())
-// app.use(cors({
-//     credentials: true,
-//     origin: (o, cb) => {
-//         // const allowed = "https://subodh-portfolio.up.railway.app"
-//         const allowed = "http://localhost:5000"
-//     }
-// }))
 app.use(express.static("public"))
 // app.use()
 app.use(express.json())
